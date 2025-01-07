f91e5a3a709ca10969aacc61689fd6e968ceec0e
André Chaibe <andrechaibedev@gmail.com>
2025-01-07 20:53:03 -0300

    Adição do projeto

---
+++ src/js/menu.js
@@ -0,0 +1,8 @@
+const miniMenu = document.querySelector(".mini-menu");
+const iconeMenu = document.getElementById("icone-menu-hamburguer");
+
+iconeMenu.addEventListener("click", () => {
+    miniMenu.classList.toggle("visivel");
+})
+
+
